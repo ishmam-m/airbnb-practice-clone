2db9a9a33323677f2791649c0e1f9619080f0c2f
dawn <mohammedishmam@gmail.com>
2024-01-07 22:27:45 -0500

    Hero section completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar.js";
+import Hero from "./components/Hero.js";
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+import photoGrid from "../images/photo-grid.png";
+
+export default function Hero() {
+  return (
+    <section class="hero-container">
+      <div className="photo-grid-container">
+        <img src={photoGrid} class="photo-grid"/>
+      </div>
+      
+      <div className="hero-info-container">
+        <h1 className="hero-header">Online Experiences</h1>
+        <p className="description">
+          Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+        </p>
+      </div>
+    </section>
+  );
+}
